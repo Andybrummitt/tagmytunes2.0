52b20aa5fb867a54f7331a3222d02a2edcbf8c42
Andy <andybrummittps4@hotmail.com>
2022-07-18 16:38:38 +0100

    changed background to gradient and removed headphones emojis from header

--- client/src/components/Header/Header.component.jsx
+++ client/src/components/Header/Header.component.jsx
@@ -4,9 +4,7 @@ import "./header.styles.scss";
 const Header = () => {
   return (
     <div className="header text-center d-flex align-items-center justify-content-center">
-        <span>&#127911;</span>
         <h1 className="text-white mt-3 mb-3">TagMyTunes.com</h1>
-        <span>&#127911;</span>
     </div>
   )
 }
